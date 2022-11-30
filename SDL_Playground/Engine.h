cc52b22618d1e2d0509daaffd823f8614c5b8166
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2022-11-30 17:28:13 +0100

    a wave of enemies is coming

---
+++ SDL_Playground/Engine.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <SDL.h>
+#include "Game.h"
+#include "Input.h"
+
+extern SDL_Renderer* renderer;
+extern Input* input;
+extern float dTime;
+
+class Engine
+{
+    SDL_Window* window;
+    Game* game;
+    const int width = 800;
+    const int height = 1000;
+    bool SDLisRunning = true;
+
+public:
+    
+    Engine()
+    {
+        SDL_Init(SDL_INIT_VIDEO);
+        window = SDL_CreateWindow(
+            "SDL2 Test",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            width, height,
+            SDL_WINDOW_SHOWN);
+        renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+
+    };
+
+    void Run();
+    
+    
+};
---
+++ SDL_Playground/Game.cpp
@@ -0,0 +1,20 @@
+﻿#include "Game.h"
+#include "Player.h"
+#include <SDL.h>
+
+#include "Engine.h"
+
+
+void Game::Update()
+{
+    if (!isRunning) { return; }
+
+    // Clear screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    if (player->isAlive) { player->Update(); }
+
+    enemies.Update();
+
+}
