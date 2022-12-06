8f5277093d81175dc299cda52c055e90695a2f8e
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2022-12-06 19:04:44 +0100

    a bag of random for the Ys

--- SDL_Playground/Enemies.h
+++ SDL_Playground/Enemies.h
@@ -1,22 +1,26 @@
 ﻿#pragma once
 #include <vector>
 #include <array>
+#include <random>
 
 struct Enemy
 {
     float x;
     float y;
+    float speed;
 
     Enemy()
     {
         x = -1;
         y = -1;
+        speed = 0;
     }
 
-    Enemy(float x, float y)
+    Enemy(float x, float y, float speed)
     {
         this->x = x;
         this->y = y;
+        this->speed = speed;
     }
 };
 
@@ -30,13 +34,17 @@ public:
     std::vector<int> recycledIndexes;
     int enemiesToSpawn = 5;
     
+    
+    std::default_random_engine generator;
+    
     Enemies()
     {
         enemies = new Enemy[capacity];
+        generator.seed(time(NULL));
     }
 
     void Update();
-    void SpawnEnemy(float x, float y);
+    void SpawnEnemy(float x, float y, float speed);
 
 private:
 };
