52be27b9cc082623a2a309442301968f06f99e32
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2022-12-08 17:23:55 +0100

    Started a bit of an overhaul

---
+++ SDL_Playground/Entity.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+struct Collider;
+struct Transform;
+
+struct Color
+{
+    int R;
+    int G;
+    int B;
+    int A = 255;
+};
+
+
+struct Entity
+{
+    int transformID;
+    int colliderID;
+    Color Color = {100, 100, 100};
+
+
+};
