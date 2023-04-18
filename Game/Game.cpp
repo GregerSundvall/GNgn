c9b36c621481e45fa8dd7419876ae8e4cda91258
GregerSundvall <71319618+GregerSundvall@users.noreply.github.com>
2023-04-18 11:18:16 +0200

    tried some different nrs for the physics

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -8,9 +8,9 @@
 
 Game::Game()
 {
-	Engine::PixelsPerMeter(50);
+	Engine::PixelsPerMeter(500);
 	auto box = Entities::Create(100, 100, 32, 32, 100, "./Game/Assets/player.png");
-	auto box2 = Entities::Create(200, 100, 32, 32, 10, "./Game/Assets/player.png");
+	auto box2 = Entities::Create(200, 100, 32, 32, 1, "./Game/Assets/player.png");
 	
 }
 
